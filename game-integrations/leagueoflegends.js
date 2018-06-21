1ff25d2a2c0fea353bd225ff1f378d0fb7410515
Nicolas <lehmannic22@gmail.com>
2018-06-20 22:21:59 -0400

    check summonerPosition data

--- game-integrations/leagueoflegends.js
+++ game-integrations/leagueoflegends.js
@@ -12,17 +12,17 @@ exports.getSummoner = async (msg, args) => {
     let versionList = await league.Static.Version.list()
     let version = versionList[0]
     let summoner = await league.Summoner.by.name(args[0])
-
-    console.log(version)
-
+    let summonerPostion = await league.LeaguePositions.by.summonerID(summoner.id)
     let level = summoner.summonerLevel
     let profileIcons = await league.Static.ProfileIcon.list()
-    console.log(JSON.stringify(summoner, undefined, 2))
+
+    console.log(JSON.stringify(summonerPostion, undefined, 2))
+
     let embed = {
         embed: {
-            image: {url: f('http://ddragon.leagueoflegends.com/cdn/%s/img/profileicon/%s.png', version, summoner.profileIconId), height: 256, width: 256}
+            title: `Summoner info:` + args[0],
+            thumbnail: {url: f('http://ddragon.leagueoflegends.com/cdn/%s/img/profileicon/%s.png', version, summoner.profileIconId), height: 256, width: 256}
         }
     }
-    console.log(embed.embed.image.url)
     bot.bot.createMessage(msg.channel.id, embed)
 }
