2d324ece59a7b731a8ef9eaca6cbf43a1b843f96
Nicolas <lehmannic22@gmail.com>
2018-06-20 22:09:55 -0400

    test

--- game-integrations/leagueoflegends.js
+++ game-integrations/leagueoflegends.js
@@ -9,7 +9,7 @@ const league = Kayn(config.RIOT_KEY)({
 })
 
 exports.getSummoner = async (msg, args) => {
-    let version = await league.Static.Version.list()
+    let version = await league.Static.Version.list()[0]
     let summoner = await league.Summoner.by.name(args[0])
 
     console.log(version)
