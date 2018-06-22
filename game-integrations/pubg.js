7ef32313a4628988f6b9f56dc34c9988569ba5e7
Nicolas <lehmannic22@gmail.com>
2018-06-22 17:22:56 -0400

    got current season check stats structure

--- game-integrations/pubg.js
+++ game-integrations/pubg.js
@@ -8,9 +8,11 @@ exports.getPlayerStats = async (msg, args) => {
     let player = await pubg.getPlayerByName('pc-na', args[0])
     let seasons = await pubg.getSeasons('pc-na')
 
-    console.log(seasons.data[seasons.data.length -1])
+    let season = seasons.data[seasons.data.length -1]
 
-    let stats = await pubg.getPlayerSeasonStats('pc-na', player.data[0].id, seasons.data[seasons.data.length -1].id)
+    let stats = await pubg.getPlayerSeasonStats('pc-na', player.data[0].id, season.id)
 
-    bot.bot.createMessage(msg.channel.id, stats.data.attributes.gameModeStats.duo)
+    console.log(JSON.stringify(stats.data))
+
+    //bot.bot.createMessage(msg.channel.id, stats.data.attributes.gameModeStats.duo)
 }
