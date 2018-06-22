1f20d75a966bbb3e289384cf81f7e9644607ab04
Nicolas <lehmannic22@gmail.com>
2018-06-22 14:52:51 -0400

    test print

--- game-integrations/pubg.js
+++ game-integrations/pubg.js
@@ -12,5 +12,5 @@ exports.test = async (msg, args) => {
     .set('Accept','application/vnd.api+json')
     .query({'filter[playerNames]':args[0]})
 
-    console.log(JSON.stringify(player.text.data, undefined, 4))
+    console.log(player.text.data)
 }
