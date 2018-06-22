7c9eeace04a9612d766d123b00345b7aedfbb825
Nicolas <lehmannic22@gmail.com>
2018-06-22 14:46:09 -0400

    Test getting pubg with superagent

--- game-integrations/pubg.js
+++ game-integrations/pubg.js
@@ -1,20 +1,16 @@
 const f = require('util').format
-const pubg = require('pubg.js');
+const client = require('superagent')
 
 const config = require('../config.json')
 const bot = require('../core.js')
 
-//add this call to functions later on to load guild configured region and support region as an argument
-const client = new pubg.Client(config.PUBG_KEY, 'pc-na');
+//Base url for PC-NA: https://api.playbattlegrounds.com/shards/pc-na
 
 exports.test = async (msg, args) => {
+    let player = await client.get(`https://api.playbattlegrounds.com/shards/pc-na/players`)
+    .set('Authorization', 'Bearer ' + config.PUBG_KEY)
+    .set('Accept','application/vnd.api+json')
+    .query({filter[playerNames]:args[0]})
 
-    client.getPlayer({name:args[0]}).then(player => {
-        try {
-            console.log(JSON.stringify(player, undefined, 3))
-        } catch (err) {
-            console.log(err)
-        }
-    })
-
+    console.log(player)
 }
