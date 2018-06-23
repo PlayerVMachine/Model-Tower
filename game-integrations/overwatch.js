8d06db55307f14a7e806e3bd24430bd9b9222ed3
Nicolas <lehmannic22@gmail.com>
2018-06-22 21:19:52 -0400

    Test

--- game-integrations/overwatch.js
+++ game-integrations/overwatch.js
@@ -2,6 +2,6 @@ const f = require('util').format
 const ow = require('overwatch-js')
 
 exports.test = async (msg, args) => {
-    let player = await ow.getOverall('pc', 'na', args[0])
+    let player = await ow.getOverall('pc', args[0])
     console.dir(player, {depth : 2, colors : true})
 }
