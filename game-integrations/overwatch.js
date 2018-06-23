101c20318baf1fb73589c0cefb3992b4bdc2bdcf
Nicolas <lehmannic22@gmail.com>
2018-06-23 19:03:57 -0400

    Catch error

--- game-integrations/overwatch.js
+++ game-integrations/overwatch.js
@@ -24,7 +24,7 @@ exports.getOverallStats = async (msg, args) => {
         let player = await ow.getModeStats(args[0], args[2], args[1])
         let account = await ow.getGeneralStats(args[0], args[1])
 
-        if (!player.career_stats['ALL HEROES'].Combat) {
+        if (!player.career_stats['ALL HEROES']) {
             bot.bot.createMessage(msg.channel.id, f(`Sorry **%s**, could not find gampeplay stats for that mode!`, msg.author.username))
             return
         }
