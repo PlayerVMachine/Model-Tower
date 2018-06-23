be8038206f5e0fdae9115464a9d295bb1a29903e
Nicolas <lehmannic22@gmail.com>
2018-06-22 20:24:50 -0400

    Added error handling to all API requests

--- game-integrations/pubg-api-wrapper.js
+++ game-integrations/pubg-api-wrapper.js
@@ -23,10 +23,10 @@ exports.getSeasons = async (region) => {
 exports.getPlayerByName = async (region, name) => {
     let url = f(`https://api.playbattlegrounds.com/shards/%s/players`, region)
     try {
-    let player = await client.get(url)
-    .set('Authorization', 'Bearer ' + config.PUBG_KEY)
-    .set('Accept','application/vnd.api+json')
-    .query({'filter[playerNames]':name})
+        let player = await client.get(url)
+        .set('Authorization', 'Bearer ' + config.PUBG_KEY)
+        .set('Accept','application/vnd.api+json')
+        .query({'filter[playerNames]':name})
 
         return JSON.parse(player.text)
     } catch (err) {
@@ -38,38 +38,44 @@ exports.getPlayerByName = async (region, name) => {
 exports.getPlayerByID = async (region, id) => {
     let url = f(`https://api.playbattlegrounds.com/shards/%s/players/%s`, region, id)
 
-    let player = await client.get(url)
-    .set('Authorization', 'Bearer ' + config.PUBG_KEY)
-    .set('Accept','application/vnd.api+json')
+    try {
+        let player = await client.get(url)
+        .set('Authorization', 'Bearer ' + config.PUBG_KEY)
+        .set('Accept','application/vnd.api+json')
 
-    if (player.code = 200)
         return JSON.parse(player.text)
-    else
+    } catch (err) {
+        console.log(err.message)
         return null
+    }
 }
 
 exports.getPlayerSeasonStats = async (region, player, season) => {
     let url = f(`https://api.playbattlegrounds.com/shards/%s/players/%s/seasons/%s`, region, player, season)
 
-    let stats = await client.get(url)
-    .set('Authorization', 'Bearer ' + config.PUBG_KEY)
-    .set('Accept','application/vnd.api+json')
+    try {
+        let stats = await client.get(url)
+        .set('Authorization', 'Bearer ' + config.PUBG_KEY)
+        .set('Accept','application/vnd.api+json')
 
-    if (stats.code = 200)
         return JSON.parse(stats.text)
-    else
+    } catch (err) {
+        console.log(err.message)
         return null
+    }
 }
 
 exports.getMatch = async (region, id) => {
     let url = f(`https://api.playbattlegrounds.com/shards/%s/matches/%s`, region, id)
 
-    let match = await client.get(url)
-    .set('Authorization', 'Bearer ' + config.PUBG_KEY)
-    .set('Accept','application/vnd.api+json')
+    try {
+        let match = await client.get(url)
+        .set('Authorization', 'Bearer ' + config.PUBG_KEY)
+        .set('Accept','application/vnd.api+json')
 
-    if (match.code = 200)
         return JSON.parse(match.text)
-    else
+    } catch (err) {
+        console.log(err.message)
         return null
+    }
 }
