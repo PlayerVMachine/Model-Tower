71b02574f488a963f22ac439b1066cef5d004b9d
Nicolas <lehmannic22@gmail.com>
2018-06-23 19:20:39 -0400

    Test Medals Command

--- game-integrations/overwatch.js
+++ game-integrations/overwatch.js
@@ -20,6 +20,8 @@ exports.getOverallStats = async (msg, args) => {
         return
     }
 
+    bot.bot.sendChannelTyping(msg.channel.id)
+
     try {
         let player = await ow.getModeStats(args[0], args[2], args[1])
         let account = await ow.getGeneralStats(args[0], args[1])
@@ -30,7 +32,7 @@ exports.getOverallStats = async (msg, args) => {
         }
 
         let qpDescription = f(`You're level **%s** and have won **%s** games after playing for **%s** hours.`, account.level, player.career_stats['ALL HEROES'].Game.GamesWon, player.career_stats['ALL HEROES'].Game.TimePlayed.slice(0, player.career_stats['ALL HEROES'].Game.TimePlayed.length - 5))
-        let cmDescription = f(`You're rank **%s** and have won **%s** out of **%s** games played in **%s** hours.`, account.rank_name, player.career_stats['ALL HEROES'].Game.GamesWon, player.career_stats['ALL HEROES'].Game.GamesPlayed, player.career_stats['ALL HEROES'].Game.TimePlayed.slice(0, player.career_stats['ALL HEROES'].Game.TimePlayed.length - 5))
+        let cmDescription = f(`You're rank **%s** and have won **%s** out of **%s** games they've played in **%s** hours.`, account.rank_name, player.career_stats['ALL HEROES'].Game.GamesWon, player.career_stats['ALL HEROES'].Game.GamesPlayed, player.career_stats['ALL HEROES'].Game.TimePlayed.slice(0, player.career_stats['ALL HEROES'].Game.TimePlayed.length - 5))
 
         let embed = {
             embed: {
@@ -58,6 +60,56 @@ exports.getOverallStats = async (msg, args) => {
 }
 
 exports.getMedals = async (msg, args) => {
+    if (args.length < 3) {
+        bot.bot.createMessage(msg.channel.id, f(`**%s**, you need to provide a username, platform, and game type!`, msg.author.username))
+        return
+    }
+
+    if (!['pc', 'xbl', 'psn'].includes(args[1])) {
+        bot.bot.createMessage(msg.channel.id, f(`**%s**, platform must be oned of: pc, xbl, psn!`, msg.author.username))
+        return
+    }
+
+    if (!['quickplay', 'competitive'].includes(args[2])) {
+        bot.bot.createMessage(msg.channel.id, f(`**%s**, game type must be quickplay or competitve!`, msg.author.username))
+        return
+    }
+
+    bot.bot.sendChannelTyping(msg.channel.id)
+
+    try {
+        let player = await ow.getModeStats(args[0], args[2], args[1])
+        let account = await ow.getGeneralStats(args[0], args[1])
+
+        if (!player.career_stats['ALL HEROES']) {
+            bot.bot.createMessage(msg.channel.id, f(`Sorry **%s**, could not find gampeplay stats for that mode!`, msg.author.username))
+            return
+        }
+
+        let awards = player.career_stats['ALL HEROES']['Match Awards']
+
+        let embed = {
+            embed: {
+                title: f(`%s's %s medals:`, args[0], args[2]),
+                thumbnail: {url: account.profile},
+                description: f(`%s has recieved %s cards and %s medals!`, args[0], awards.Cards, awards.Medals),
+                fields: [
+                    {name: `Gold Medals`, value: awards.MedalsGold, inline: false},
+                    {name: `Silver Medals`, value: awards.MedalsSilver, inline: false},
+                    {name: `Bronze Medals`, value: awards.MedalsBronze, inline: false}
+                ]
+            }
+        }
+
+        bot.bot.createMessage(msg.channel.id, embed)
+
+    } catch (err) {
+        console.log(err)
+        if (err == 'PLAYER_NOT_EXIST') {
+            bot.bot.createMessage(msg.channel.id, f(`Sorry **%s**, could not find results for that user on platform %s!`, msg.author.username, args[1]))
+            return
+        }
+    }
 
 }
 
