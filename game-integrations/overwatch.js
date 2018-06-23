ac07f1401869353aba27b23233e1e85ec2786a37
Nicolas <lehmannic22@gmail.com>
2018-06-22 21:39:36 -0400

    Print overall stats in Discord

--- game-integrations/overwatch.js
+++ game-integrations/overwatch.js
@@ -1,7 +1,9 @@
 const f = require('util').format
 const ow = require('owapi')
 
+const bot = require('../core.js')
+
 exports.test = async (msg, args) => {
     let player = await ow.getModeStats(args[0], 'quickplay', 'pc')
-    console.dir(player.career_stats, {depth : 2, colors : true})
+    bot.bot.createMessage(msg.channel.id, '```\n' + JSON.stringify(player.career_stats['ALL HEROES'], undefined, 4) + '\n```')
 }
