f35c29db5e9e02dc573cb3b49f24bbca816bd17f
Nicolas <lehmannic22@gmail.com>
2018-06-22 21:35:21 -0400

    Test getting stats from this api

--- game-integrations/overwatch.js
+++ game-integrations/overwatch.js
@@ -2,6 +2,6 @@ const f = require('util').format
 const ow = require('owapi')
 
 exports.test = async (msg, args) => {
-    let player = await ow.getGeneralStats(args[0], 'pc')
+    let player = await ow.getModeStats(args[0], 'quickplay', 'pc')
     console.dir(player, {depth : 2, colors : true})
 }
