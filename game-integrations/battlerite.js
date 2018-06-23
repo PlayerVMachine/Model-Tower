96bb7fb92cf675a74761e9efe066edd46cdd4846
Nicolas <lehmannic22@gmail.com>
2018-06-22 20:51:36 -0400

    Await the response

--- game-integrations/battlerite.js
+++ game-integrations/battlerite.js
@@ -4,7 +4,7 @@ const config = require('../config.json')
 //https://api.dc01.gamelockerapp.com/shards/global/players?filter[playerIds]=player1Id,player2Id
 
 exports.test = async (msg, args) => {
-    let player = request.get('https://api.dc01.gamelockerapp.com/shards/global/players')
+    let player = await request.get('https://api.dc01.gamelockerapp.com/shards/global/players')
     .set('Authorization', 'Bearer ' + config.BATTLERITE_KEY)
     .set('Accept', 'application/vnd.api+json')
     .query({'filter[playerNames]':args[0]})
