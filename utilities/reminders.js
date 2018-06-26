8c830bf8381b8fa4cff34414d37ef081710c1126
Nicolas <lehmannic22@gmail.com>
2018-06-25 23:03:12 -0400

    Test setting future reminder time

--- utilities/reminders.js
+++ utilities/reminders.js
@@ -22,16 +22,41 @@ exports.remindMe = async (msg, args) => {
 
         let splitIndex = msg.content.split(' ').lastIndexOf('in')
 
-        let reminder = msg.content.split(' ').slice(1, splitIndex)
+        let reminder = msg.content.split(' ').slice(1, splitIndex).join(' ')
         let rawTime = msg.content.split(' ').slice(splitIndex + 1).join()
         let dmChannel = await bot.bot.getDMChannel(msg.author.id)
 
         //set the time unit for minute, day, week, month, or year
-        let timeUnit = rawTime.charAt(rawTime.length - 1).match(/[mMdyw]/) ? rawTime.charAt(rawTime.length - 1) : 'm'
+        let timeUnit = rawTime.charAt(rawTime.length - 1).match(/[mMhdyw]/) ? rawTime.charAt(rawTime.length - 1) : 'm'
 
         let timeMagnitude = !parseInt(rawTime) ? parseInt(rawTime.slice(0, rawTime.length -1)) : parseInt(rawTime)
 
         bot.bot.createMessage(msg.channel.id, f('magnitude: %d, unit: %s, reminder: %s', timeMagnitude, timeUnit, reminder))
+
+        //breakdown date
+        let date = new Date()
+        let min = date.getMinutes()
+        let hour = date.getHours()
+        let day = date.getDate()
+        let month = date.getMonth()
+        let year = date.getFullYear()
+
+        if (timeUnit == 'm') {
+            min += timeMagnitude
+        } else if (timeUnit == 'h') {
+            hour += timeMagnitude
+        } else if (timeUnit == 'd') {
+            day += timeMagnitude
+        } else if (timeUnit == 'w') {
+            day += (7 * timeMagnitude)
+        } else if (timeUnit == 'M') {
+            month += timeMagnitude
+        } else if (timeUnit == 'y') {
+            year += timeMagnitude
+        }
+
+        let reminderTime = new Date(remYear, remMonth, remDay, remHours, remMins)
+        console.log(date + ' -> ' + reminderTime)
 
 /*        let reminderObj = {
             sendTo: dmChannel.id,
