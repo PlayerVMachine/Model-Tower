0d01e50eb4362823d6aa5b4fa785d1c718a1929e
Nicolas <lehmannic22@gmail.com>
2018-07-07 15:07:54 -0400

    test

--- utilities/tools.js
+++ utilities/tools.js
@@ -2,7 +2,7 @@
 const MongoClient = require('mongodb').MongoClient
 const f = require('util').format
 
-const bot = require('../core.js').bot
+const { bot } = require('../core.js')
 const config = require('../config.json')
 
 // mongodb login
