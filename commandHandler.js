aa4d318c5ab5df76a6b0db7af26945e14d9176b3
Nicolas <lehmannic22@gmail.com>
2018-07-07 19:58:50 -0400

    fix typo

--- commandHandler.js
+++ commandHandler.js
@@ -23,7 +23,7 @@ const config = require('./config.json')
 const bot = require('./core.js')
 
 const userWait = async (command, time, msg) => {
-    memcached.get(command + msg.author.id, async (err, res)) {
+    memcached.get(command + msg.author.id, async (err, res)) => {
         if (!err && !res) {
             let message = await bot.bot.createMessage(msg.channel.id, f(`Sorry %s, you need to wait %d seconds before using %s again`, msg.author.username, (time/1000), command))
             setTimeout(() => {message.delete(`Delete cooldown warning`)}, 3000)
