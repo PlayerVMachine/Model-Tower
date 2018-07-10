6a5822f939847bb4a7db4280a82041556f8fc2b4
Nicolas <lehmannic22@gmail.com>
2018-07-09 22:49:09 -0400

    test

--- game-integrations/steam-api-wrapper.js
+++ game-integrations/steam-api-wrapper.js
@@ -30,7 +30,7 @@ const getGameIDByName = (name) => {
 
     request(searchURL, (err, res, html) => {
         const $  = cheerio.load(html)
-        let test = $('a').prop('data-ds-appid')
+        let test = $('a').data('data-ds-appid')
         console.dir(test)
     })
 
