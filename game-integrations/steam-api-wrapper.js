2d3bc4e40d81bab2c14551543fdfa3f33f58bb1c
Nicolas <lehmannic22@gmail.com>
2018-07-10 18:11:40 -0400

    fix typo

--- game-integrations/steam-api-wrapper.js
+++ game-integrations/steam-api-wrapper.js
@@ -59,7 +59,7 @@ const getNewsForApp = async (name, count, maxLength) => {
     }
 
     let appID = null
-    console.log(name.match(/\D/g)
+    console.log(name.match(/\D/g))
     if (name.match(/\D/g) == null) {
         console.log('reached')
         //contains non digit characters so assume it's a name
