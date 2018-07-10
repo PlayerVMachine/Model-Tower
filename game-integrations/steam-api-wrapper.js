d011a6ab61546e1487f1885e2f10a36d7510374d
Nicolas <lehmannic22@gmail.com>
2018-07-10 17:49:22 -0400

    Test failing if case

--- game-integrations/steam-api-wrapper.js
+++ game-integrations/steam-api-wrapper.js
@@ -59,7 +59,7 @@ const getNewsForApp = async (name, count, maxLength) => {
     }
 
     let appID = null
-    if (!name.match(/\D/g)) {
+    if (name.match(/\D/g) == null) {
         //contains non digit characters so assume it's a name
         appID = await getGameIDByName(name)
         if (!appID) {
