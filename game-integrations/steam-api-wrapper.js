a69aa5ab30caca9814485156603f7aed2f779f3f
Nicolas <lehmannic22@gmail.com>
2018-07-12 18:50:34 -0400

    test

--- game-integrations/steam-api-wrapper.js
+++ game-integrations/steam-api-wrapper.js
@@ -407,7 +407,7 @@ const getAssetUrl = async (app) => {
 }
 
 async function test () {
-    let res = await getSchemaForGame('7670')
+    let res = await getSchemaForGame('borderlands')
     console.dir(res, {depth: 4})
 }
 test()
